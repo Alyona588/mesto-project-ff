c5a4664844070a5a44b178e936e84e404735d61b
Alyona Popova <popova.alyona.97@mail.ru>
2024-02-07 15:00:39 +0300

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector(".places");
+const placesList = document.querySelector(".places__list");
+const addButton = document.querySelector(".profile__add-button");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function createCard(initialCards, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__title").textContent = initialCards.name;
+  cardElement.querySelector(".card__image").src = initialCards.link;
+  cardElement.querySelector(".card__image").alt = initialCards.name;
+
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+
+  return cardElement;
+}
+
+function deleteCard(item) {
+  item.remove();
+}
+
+addButton.addEventListener("click", function () {
+  initialCards.forEach((item) => {
+    placesList.append(createCard(item, deleteCard));
+  });
+});
