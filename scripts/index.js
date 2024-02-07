3fba91856b206b292a1a1358c495f91c1d5e9b76
Alyona Popova <popova.alyona.97@mail.ru>
2024-02-07 16:23:56 +0300

    third_commit

--- scripts/index.js
+++ scripts/index.js
@@ -13,13 +13,13 @@ const placesList = document.querySelector(".places__list");
 const addButton = document.querySelector(".profile__add-button");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function createCard(initialCards, deleteCard) {
+function createCard(cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
-  cardElement.querySelector(".card__title").textContent = initialCards.name;
-  cardElement.querySelector(".card__image").src = initialCards.link;
-  cardElement.querySelector(".card__image").alt = initialCards.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
 
   deleteButton.addEventListener("click", () => deleteCard(cardElement));
 
@@ -30,8 +30,6 @@ function deleteCard(item) {
   item.remove();
 }
 
-addButton.addEventListener("click", function () {
-  initialCards.forEach((item) => {
-    placesList.append(createCard(item, deleteCard));
-  });
+initialCards.forEach((item) => {
+  placesList.append(createCard(item, deleteCard));
 });
